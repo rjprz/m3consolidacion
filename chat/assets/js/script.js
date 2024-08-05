b6b5ed0da00d122f9e99f6709b0fbbcb3f85bcb8
rjpr <ricardo.parrague@usm.cl>
2024-08-04 23:48:11 -0400

    delta

--- chat/assets/js/script.js
+++ chat/assets/js/script.js
@@ -1,29 +1,32 @@
 const chatIcon = document.querySelector(".navbar__chats");
-const chatSearch = document.querySelector(".navbar__search")
+const chatSearch = document.querySelector(".navbar__search");
 //const chatIcon = document.querySelector(".navbar");
 
 const ventanaChats = document.querySelector(".chats-recientes-contenedor");
 const botonBack = document.querySelector(".navbar__back");
 const menu = document.querySelector(".navbar__menu");
 const navbar = document.querySelector("nav");
-const  chatRecientes = document.querySelectorAll(".chat-recientes__chats")
+const chatRecientes = document.querySelectorAll(".chat-recientes__chats");
 
-const dropDownButton = document.querySelector(".chat__icon-clip")
-const dropDownMenu = document.querySelector(".chat__drop-down")
+const dropDownButton = document.querySelector(".chat__icon-clip");
+const dropDownMenu = document.querySelector(".chat__drop-down");
 
-chatIcon.addEventListener("click", function () {
+const ventanaAbajo = function () {
   chatIcon.style.display = "none";
   chatSearch.style.display = "block";
   botonBack.style.display = "block";
   ventanaChats.style.transform = "translateY(0)";
-});
-
-botonBack.addEventListener("click", function () {
+};
+const ventanaArriba = function () {
   botonBack.style.display = "none";
   chatIcon.style.display = "block";
   chatSearch.style.display = "none";
   ventanaChats.setAttribute("style", "transform: translateY(-120vh)");
-});
+};
+
+chatIcon.addEventListener("click", ventanaAbajo);
+
+botonBack.addEventListener("click", ventanaArriba);
 
 menu.addEventListener("click", function () {
   navbar.classList.toggle("navbar-open");
@@ -37,9 +40,24 @@ chatRecientes.forEach((chat) => {
   });
 });
 
-dropDownButton.addEventListener("mouseover", function(){
+dropDownButton.addEventListener("mouseover", function () {
   dropDownMenu.style.display = "block";
-})
-dropDownButton.addEventListener("mouseout", function(){
+});
+dropDownButton.addEventListener("mouseout", function () {
   dropDownMenu.style.display = "none";
-})
+});
+
+//evitar bug al abrir menu chats en modo mobil y luego pasar a modo desktop
+
+const mql = window.matchMedia("(min-width: 576px)");
+
+mql.onchange = (e) => {
+  if (e.matches) {
+    /* the viewport is 576px pixels wide or more */
+    ventanaAbajo();
+    botonBack.style.display = "none";
+  } else {
+    /* the viewport is more than 600 pixels wide */
+    ventanaArriba();
+  }
+};
