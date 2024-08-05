93fc974a39a05bd20f443ba2677b33113fddf296
rjpr <ricardo.parrague@usm.cl>
2024-08-04 20:41:38 -0400

    beta por si es corta la luz

--- chat/assets/js/script.js
+++ chat/assets/js/script.js
@@ -1,13 +1,16 @@
 const chatIcon = document.querySelector(".navbar__chats");
+const chatSearch = document.querySelector(".navbar__search")
 //const chatIcon = document.querySelector(".navbar");
 
 const ventanaChats = document.querySelector(".chats-recientes-contenedor");
 const botonBack = document.querySelector(".navbar__back");
 const menu = document.querySelector(".navbar__menu");
 const navbar = document.querySelector("nav");
+const  chatRecientes = document.querySelectorAll(".chat-recientes__chats")
 
 chatIcon.addEventListener("click", function () {
   chatIcon.style.display = "none";
+  chatSearch.style.display = "block";
   botonBack.style.display = "block";
   ventanaChats.style.transform = "translateY(0)";
 });
@@ -15,9 +18,18 @@ chatIcon.addEventListener("click", function () {
 botonBack.addEventListener("click", function () {
   botonBack.style.display = "none";
   chatIcon.style.display = "block";
+  chatSearch.style.display = "none";
   ventanaChats.setAttribute("style", "transform: translateY(-120vh)");
 });
 
 menu.addEventListener("click", function () {
   navbar.classList.toggle("navbar-open");
+});
+
+chatRecientes.forEach((chat) => {
+  chat.addEventListener("click", function () {
+    console.log("clikiti clak clakkkk ckaaaakasjdahjsd");
+    chat.setAttribute("style", "font-weight: 400;");
+    console.log(chat);
+  });
 });
